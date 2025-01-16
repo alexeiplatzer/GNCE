63f4061b5bb24419abc8487df3462151dc4938a8
Alexei Platzer <ge65tid@tum.de>
2025-01-16 21:46:51 +0100

    change datasets folder to be in parent repository of the project

--- src/cardinality_estimation/constants.py
+++ src/cardinality_estimation/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 PROJECT_ROOT_PATH = Path(__file__).parent.parent.parent  # should be the parent of src
-DATASETS_PATH = PROJECT_ROOT_PATH / "Datasets"
+DATASETS_PATH = PROJECT_ROOT_PATH.parent / "Datasets"
 PACKAGE_PATH = PROJECT_ROOT_PATH / "src" / "cardinality_estimation"
 GNCE_PATH = PACKAGE_PATH / "GNCE"
 
